fedea348495ee2be12ed4b9fece25ce6ddeb7d20
Selbi Ereshova <selbi.ereshova@ug.bilkent.edu.tr>
2022-12-09 18:52:51 +0300

    Added initial file structure

---
+++ public/js/database/basic-queries.js
@@ -0,0 +1,14 @@
+var neo4j = require("neo4j-driver");
+
+
+var basicQueries = {
+    getAuthorAndPapers: function()
+    {
+
+    },
+    getPaperAndPapers: function()
+    {
+
+    }
+}
+module.exports = basicQueries;
