a1973842790b3e6a35f3b932beb13bb5562081d4
Zehra  Erdem <zehra.erdem@ug.bilkent.edu.tr>
2022-12-10 15:40:41 +0300

    Recat is created

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          Learn React
+      </header>
+      <p>
+        Demo
+      </p>
+    </div>
+  );
+}
+
+export default App;
