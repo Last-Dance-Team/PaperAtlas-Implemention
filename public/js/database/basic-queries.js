5512fc3951be9cb9023a21f3dac2fac1dff59e93
Selbi Ereshova <selbi.ereshova@ug.bilkent.edu.tr>
2022-12-18 20:40:19 +0300

    Removed limit to get all data

--- public/js/database/basic-queries.js
+++ public/js/database/basic-queries.js
@@ -6,14 +6,12 @@ var basicQueries = {
   getAuthorAndPapers: function (author) {
     return `MATCH (a:Author) WHERE a.name CONTAINS "${author}"
             WITH a 
-            LIMIT 25  
             MATCH (:Author {authorId: a.authorId})-[r]-(p:Paper) 
             RETURN a,p,r`;
   },
   getPaperAndPapers: function (paper, lengthLimit) {
     return `MATCH (p:Paper) WHERE p.title CONTAINS "${paper}"
             WITH p 
-            LIMIT 50  
             MATCH (:Paper {paperId: p.paperId})-[r*..${lengthLimit}]-(pp:Paper) 
             RETURN p,pp,r`;
   },
